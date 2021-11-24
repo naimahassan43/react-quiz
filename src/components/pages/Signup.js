40cdb0776b96073f28d93a36dbcd687072e692d3
Naima Hassan <naima.hassan434@gmail.com>
2021-11-24 23:50:55 +0000

    textinput, button, checkbox components

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,7 +1,10 @@
 import React from "react";
 import classes from "../../styles/Signup.module.css";
+import Button from "../Button";
+import CheckBox from "../CheckBox";
 import Form from "../Form";
 import Illustration from "../Illustration";
+import TextInput from "../TextInput";
 
 export default function Signup() {
   return (
@@ -9,7 +12,34 @@ export default function Signup() {
       <h1>Create an account</h1>
       <div className={classes.column}>
         <Illustration />
-        <Form className={`${classes.signup}`} />
+        <Form className={`${classes.signup}`}>
+          <TextInput type="text" placeholder="Enter name" icon="person" />
+
+          <TextInput
+            type="text"
+            placeholder="Enter  email"
+            icon="alternate_email"
+          />
+
+          <TextInput
+            type="password"
+            placeholder="Enter  password"
+            icon="lock"
+          />
+
+          <TextInput
+            type="password"
+            placeholder="Confirm  password"
+            icon="lock_clock"
+          />
+
+          <CheckBox text="I agree to the Terms &amp; Conditions" />
+        </Form>
+        <Button>Submit now</Button>
+        <div className="info">
+          Already have an account?
+          <a href="login.html">Login</a> instead.
+        </div>
       </div>
     </>
   );
