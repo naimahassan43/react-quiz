388cffe43b561991325fe8473bc346dbca3c831f
Naima Hassan <naima.hassan434@gmail.com>
2021-11-24 23:07:39 +0000

    Signup and Illustration components

---
+++ src/components/Illustration.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "../../styles/Illustration.module.css";
+import signupImg from "../assets/images/signup.svg";
+export default function Illustration() {
+  return (
+    <div className={classes.illustration}>
+      <img src={signupImg} alt="Signup" />
+    </div>
+  );
+}
---
+++ src/components/pages/Signup.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "../../styles/Signup.module.css";
+import Form from "../Form";
+import Illustration from "../Illustration";
+
+export default function Signup() {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <div className={classes.column}>
+        <Illustration />
+        <Form className={`${classes.signup} form`} />
+      </div>
+    </>
+  );
+}
