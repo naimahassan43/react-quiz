4c7d7ff6f3a69526461344dc1c24a43528c56615
Naima Hassan <naima.hassan434@gmail.com>
2021-11-25 10:02:38 +0000

    Answers and Progressbar components

---
+++ src/components/Answers.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CheckBox from "./CheckBox";
+import classes from "../styles/Answers.module.css";
+export default function Answers() {
+  return (
+    <div className={classes.answers}>
+      <CheckBox className={classes.answer} text="Test Answer" />
+    </div>
+  );
+}
