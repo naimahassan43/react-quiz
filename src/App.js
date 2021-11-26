d4c104c2fafaec34d6e3e4699a633b4da83f48b5
Naima Hassan <naima.hassan434@gmail.com>
2021-11-26 00:21:50 +0000

    firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./components/pages/Login";
 import Quiz from "./components/pages/Quiz";
 import Home from "./components/pages/Home";
 import Signup from "./components/pages/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
